ac6fe83c14f545e1c1a2d1f66dea341533f54165
kz_morita <kz.morit@gmail.com>
2019-10-22 16:42:13 +0900

    [WIP] Change background color to white

--- haribOS/bootpack.c
+++ haribOS/bootpack.c
@@ -1,8 +1,14 @@
 extern void _io_hlt(void);
+extern void _write_mem8(int addr, int data);
 
 void HariMain(void) {
 
-fin:
-    _io_hlt();
-    goto fin;
+    int i;
+    for (i = 0xa0000; i <= 0xaffff; i++) {
+        _write_mem8(i, 15); // MOV BYTE [i], 15
+    }
+
+    for (;;) {
+        _io_hlt();
+    }
 }
--- haribOS/nasmfunc.asm
+++ haribOS/nasmfunc.asm
@@ -3,7 +3,17 @@
 
 section: .text
 	GLOBAL	_io_hlt
+	GLOBAL	_write_mem8
 
-_io_hlt:	; void io_hlt(void);
+_io_hlt:	; void _io_hlt(void);
 	HLT
 	RET
+
+_write_mem8:	; void _write_mem8(int addr, int data);
+; int型 (4 Byte) の data の下位1Byteのみを addr に書き込む
+	MOV		ECX, DWORD [ESP+4]	; [ESP+4] ~ [ESP+7] に (int)addr が入っている, 32bit アドレッシング
+	MOV		AL,  BYTE  [ESP+8]	; [ESP+8] ~ [ESP+11] に (int) data が入っている. 
+
+	MOV		BYTE [ECX], AL
+	RET
+
